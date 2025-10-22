6807738bbae8c8ad5e8732027c7b9ca034eb6d47
Dewan Mahmud Rocky <rockyit10@gmail.com>
2025-10-22 01:29:48 -0400

    Initial_Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log('Hello World');
+//let name ='Rocky';
+//console.log(name);
+
+// Can not be a reserved keyword
+// Should be meaningful
+// Cannot start with a number 
+// Cannot contain a space or hyphen
+// Cannot start with a number
+// Case sensitive   
+// Example: firstName, FirstName, firstname
+// Use camelCase for naming variables 
+// Example: firstName, lastName, dateOfBirth
+// Use const for constants, let for variables that can change
+// Example: const pi = 3.14, let age = 25
+// Use meaningful names for variables
+
+
+let firstName = 'Rocky';
+let lastName = 'Dewan';
+console.log(firstName);
+console.log(lastName); 
+console.log(firstName + ' ' + lastName);
+
