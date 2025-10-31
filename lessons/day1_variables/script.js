0dd64b773a9c44f34a09fb6e1c478143742ff871
Dewan Mahmud Rocky <rockyit10@gmail.com>
2025-10-30 23:50:05 -0400

    test_codes2

--- lessons/day1_variables/script.js
+++ lessons/day1_variables/script.js
@@ -3,3 +3,10 @@
 let name = "Rocky";
 const program = "Per Scholas";
 let age = 27;
+
+console.log("Name:", name);
+console.log("Program:", program);
+console.log("Age:", age);
+// You can change the value of a variable declared with let:
+age = 28;
+console.log("Updated Age:", age);
